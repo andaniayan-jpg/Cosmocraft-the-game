3a52aa449f4e6a37366dfa19aad632819df9b73a
Ayan Andani <andaniayan@gmai.com>
2026-07-08 16:39:02 +0530

    wrote half of thr game function in game.js file

---
+++ game.js
@@ -0,0 +1,28 @@
+var moneyText = document.getElementById("moneyText");
+var iroText = document.getElementById("ironText");
+var crystalText = document.getElementById("crystalText");
+var goldText = document.getElementById("goldText");
+var fuelText = document.getElementById("fuelText");
+var storageText = document.getElementById("heatText");
+var heatText = document.getElementById("heatText");
+var planetName = document.getElementById("planetName");
+var planetDescription = document.getElementById("planetDescription");
+var planetStatus = document.getElementById("planetStatus");
+var planetCircle = document.getElementById("planetCircle");
+
+
+var messageText = document.getElementById("messageText");
+var logBox = document.getElementById("logBox");
+var mineBtn = document.getElementById("mineBtn");
+
+var sellBtn = document.getElementById("sellBtn");
+var coolBtn = document.getElementById("collBtn");
+var refuelBtn = document.getElementById("refuelBtn");
+
+var upgradeDrillBtn = document.getElementById("upgradeDrillBtn");
+var upgradeStorageBtn = document.getElementById("upgradeStorageBtn");
+var upgradeFuelBtn = document.getElementById("upgradeFuelBtn");
+var upgradeAutoMinerBtn = document.getElementById("upgradeAutoMinerBtn");
+
+var drillLevelText = document.getElementById("drillLevelText");
+var drillCostText = document.etElementById("drillCostText");
